ca0baebee43fa8454c162632fd604e8bb73ef482
wcyjames <wcy_james@outlook.com>
2021-01-05 14:17:47 +0800

    e

--- models/pointnet2_part_seg_ssg.py
+++ models/pointnet2_part_seg_ssg.py
@@ -32,20 +32,20 @@ class get_model(nn.Module):
         else:
             l0_points = xyz
             l0_xyz = xyz
-        l1_xyz, l1_points = self.sa1(l0_xyz, l0_points)
-        print(l1_xyz.shape, l1_points.shape)
-        l2_xyz, l2_points = self.sa2(l1_xyz, l1_points)
-        print(l2_xyz.shape, l2_points.shape)
-        l3_xyz, l3_points = self.sa3(l2_xyz, l2_points)
-        print(l3_xyz.shape, l3_points.shape)
+        l1_xyz, l1_points = self.sa1(l0_xyz, l0_points)     # torch.Size([16, 3, 512]) torch.Size([16, 128, 512]) N = 512
+
+        l2_xyz, l2_points = self.sa2(l1_xyz, l1_points)  # torch.Size([16, 3, 128]) torch.Size([16, 256, 128]) N = 128
+
+        l3_xyz, l3_points = self.sa3(l2_xyz, l2_points)  # torch.Size([16, 3, 1]) torch.Size([16, 1024, 1]) N = 1
         # Feature Propagation layers
         l2_points = self.fp3(l2_xyz, l3_xyz, l2_points, l3_points)  # torch.Size([16, 256, 128])
         l1_points = self.fp2(l1_xyz, l2_xyz, l1_points, l2_points)  # [16, 128, 512])
         cls_label_one_hot = cls_label.view(B,16,1).repeat(1,1,N)     # torch.Size([16, 16, 2048])
 
-        l0_points = torch.cat([cls_label_one_hot,l0_xyz,l0_points],1) # torch.Size([16, 25, 2048])
+        l0_points = torch.cat([cls_label_one_hot,l0_xyz,l0_points],1)  # torch.Size([16, 25, 2048])
 
-        l0_points = self.fp1(l0_xyz, l1_xyz, l0_points, l1_points) # torch.Size([16, 128, 2048])
+        l0_points = self.fp1(l0_xyz, l1_xyz, l0_points, l1_points)  # torch.Size([16, 128, 2048])
+        print(l0_points.shape)
         # FC layers
         feat =  F.relu(self.bn1(self.conv1(l0_points)))
         x = self.drop1(feat)
